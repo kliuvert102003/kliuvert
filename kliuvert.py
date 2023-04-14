22256c9579d9d7c4d51edf9c0c8e65dae7de55e1
Kliuvert Garcia <kliuvertgarciagarciaquinonez@gmail.com>
2023-04-14 16:27:42 -0500

    pais favorito

--- kliuvert.py
+++ kliuvert.py
@@ -1,5 +1,10 @@
+import random
+
 nombre = input("introduce tu nombre: ")
 print("tu nombre es",nombre)
 
 edad = int(input("introduce tu edad: "))
 print("tu edad es ", edad)
+
+paises = ["Canada", "Bulgaria", "Polonia","Alemania"]
+print("tu pais favorito es",random.choice(paises))
