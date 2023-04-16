5a70ba02acc270add62c0b1614a9a87452b7c41a
Kliuvert Garcia <kliuvertgarciagarciaquinonez@gmail.com>
2023-04-15 22:41:26 -0500

    Condicionales para elegir tu destino

--- ejercicio.py
+++ ejercicio.py
@@ -2,3 +2,17 @@
 usuario = input("Introduce una Ciudad: ")
 ciudades = ["Bogota", "Medellin","Cali","Manizales"]
 
+if usuario == ciudades[0]:
+    print("tu destino es", ciudades[0])
+
+elif usuario == ciudades[1]:
+    print("tu destino es", ciudades[1])
+
+elif usuario == ciudades[2]:
+    print("tu destino es", ciudades[2])
+
+elif usuario == ciudades[3]:
+    print("tu destino es", ciudades[3])
+
+else:
+    print("¡Error!")
